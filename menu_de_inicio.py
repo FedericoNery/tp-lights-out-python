00eddfc2dc9535edd8f097ecd91dc11f2ab0ae09
FedericoNery <fedegnery@gmail.com>
2017-04-23 15:33:45 -0300

    Desarrollo inicial del menú de inicio. No se implementa la limpieza de pantalla, falta definir la función

---
+++ menu_de_inicio.py
@@ -0,0 +1,15 @@
+print("BIENVENIDOS AL JUEGO\n")
+print("1.JUGAR \n")
+print("2.SALIR \n")
+while((valor!='1') or (valor!='2')):
+    valor=input("Ingrese el numero correspondiente a la accion que desea realizar ")
+print ("1.MODO ALEATORIO\n")
+print ("2.MODO PREDETERMINADO\n")
+while((valor!='1') or (valor!='2')):
+    valor=input("Ingrese el numero correspondiente a la accion que desea realizar ")
+    if(valor=='1'):
+    #Llamar a la funcion modo aleatorio
+    elif(valor=='2'):
+    #Llamar a la funcion modo predeterminado
+
+
