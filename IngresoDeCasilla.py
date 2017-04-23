334c2ef5772f5f3b3a6abec0b8256ef6d01b1285
FedericoNery <fedegnery@gmail.com>
2017-04-23 14:23:30 -0300

    Se realizó la función de forma completa,  limitando el ingreso solo de la forma "letra-número" respetando las dimensiones del tablero(si se ingresa una casilla no existente se vuelve a pedir el ingreso). No acepta caracteres especiales. La casilla es una lista con caracteres(string)

--- IngresoDeCasilla.py
+++ IngresoDeCasilla.py
@@ -3,12 +3,15 @@ def validacionIngresoDeCasillero(dimension_tablero):
     se verifica que la casilla pertenezca al tablero y que el ingreso no sea erroneo (mas caracteres o la inversion
     del orden de ingreso letra-numero(numero-letra)"""
     validacion=False
+    dimension_fila=str(unichr(dimension_tablero+48))
+    dimension_columna=str(unichr(dimension_tablero+64))
+
     while(validacion==False):
         casilla = raw_input("Ingrese la casilla de la luz que desea apagar ")
         casilla = casilla.upper()
         if(len(casilla)==2):
-            if((casilla[0]>='A') and (casilla[0]<='Z')):
-                if((casilla[1]>='1') and (casilla[1]<='9')):
+            if((casilla[0]>='A') and (casilla[0]<=dimension_columna)):
+                if((casilla[1]>='1') and (casilla[1]<=dimension_fila)):
                     validacion=True
                     return (casilla)
                 else:
@@ -20,4 +23,4 @@ def validacionIngresoDeCasillero(dimension_tablero):
         else:
             validacion= False
             print ("Error ")
-validacionIngresoDeCasillero(0)
+validacionIngresoDeCasillero(5)
