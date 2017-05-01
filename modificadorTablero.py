5f7c09b6a2b431bcf449cc3c6131173ac58ff91a
FedericoNery <fedegnery@gmail.com>
2017-05-01 20:11:33 -0300

    Se agrega la modificación requerida para que, cuando se desee apagar una luz previamente apagada, esta se encienda y sus aledañas cambien de estado(de encendida a apagada,o, apagada a encendida)

--- modificadorTablero.py
+++ modificadorTablero.py
@@ -8,22 +8,32 @@ def modificoTablero(Diccionario,Casilla):
     """ Casilla Central"""
     if(Diccionario.get(Casilla,[])=='O'):
         Diccionario[Casilla]='.'
+    else:
+        Diccionario[Casilla]='O'
     """ Casilla a la izquierda de la Central """
     casillaALaIzquierda= chr(ord(columnaCasillaCentral)-1)+filaCasillaCentral
-    if(Diccionario.get(casillaALaIzquierda,[])):
+    if(Diccionario.get(casillaALaIzquierda,[])=='O'):
         Diccionario[casillaALaIzquierda]='.'
+    else:
+        Diccionario[casillaALaIzquierda]='O'
     """ Casilla a la derecha de la Central """
     casillaALaDerecha = chr(ord(columnaCasillaCentral)+1)+filaCasillaCentral
-    if(Diccionario.get(casillaALaDerecha, [])):
+    if(Diccionario.get(casillaALaDerecha,[])=='O'):
         Diccionario[casillaALaDerecha]='.'
+    else:
+        Diccionario[casillaALaDerecha]='O'
     """Casilla superior a la Central"""
     casillaSuperior= columnaCasillaCentral + str(int(filaCasillaCentral)-1)
-    if(Diccionario.get(casillaSuperior, [])=='O'):
+    if(Diccionario.get(casillaSuperior,[])=='O'):
         Diccionario[casillaSuperior]='.'
+    else:
+        Diccionario[casillaSuperior]='O'
     """Casilla inferior a la Central"""
     casillaInferior = columnaCasillaCentral + str(int(filaCasillaCentral)+1)
     if(Diccionario.get(casillaInferior, [])=='O'):
         Diccionario[casillaInferior]='.'
+    else:
+        Diccionario[casillaInferior]='O'
     return (Diccionario)
 
 
