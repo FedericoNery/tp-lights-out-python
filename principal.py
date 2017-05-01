7156a4ff5162d307e261b99e59edbd0f1edce073
FedericoNery <fedegnery@gmail.com>
2017-05-01 16:28:43 -0300

    Se unen las partes del menú de inicio junto a la generación de los respectivos tableros según el modo Aleatorio o Predeterminado de juego. Solo un ciclo

--- principal.py
+++ principal.py
@@ -1,5 +1,17 @@
 import menu_de_inicio
+import ingreso_de_casilla
+import modificadorTablero
+import mostrar_tablero
+import niveles_predeterminados
+import niveles_aleatorios
 
 def principal():
-    menu_de_inicio.menu_de_inicio()
+    modoYDimension=menu_de_inicio.menu_de_inicio()
+    if(modoYDimension[0]=="Predeterminado"):
+        diccionario_tablero=niveles_predeterminados.niveles_predeterminados(1)
+        mostrar_tablero.imprimir_Tablero(diccionario_tablero,modoYDimension[1])
+    elif(modoYDimension[0]=="Aleatorio"):
+        diccionario_tablero=niveles_aleatorios.generarTablerosConLuces(modoYDimension[1])
+        mostrar_tablero.imprimir_Tablero(diccionario_tablero,modoYDimension[1])
+
 principal()
