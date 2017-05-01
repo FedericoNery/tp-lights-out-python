b7d91f3269e7c90bdf03410ae7f52ae907114f34
FedericoNery <fedegnery@gmail.com>
2017-05-01 15:08:15 -0300

    Se actualizó la función para que sea compatible con la versión de Python 3.5

--- ingreso_de_casilla.py
+++ ingreso_de_casilla.py
@@ -3,11 +3,11 @@ def validacionIngresoDeCasillero(dimension_tablero):
     se verifica que la casilla pertenezca al tablero y que el ingreso no sea erroneo (mas caracteres o la inversion
     del orden de ingreso letra-numero(numero-letra)"""
     validacion=False
-    dimension_fila=str(unichr(dimension_tablero+48))
-    dimension_columna=str(unichr(dimension_tablero+64))
+    dimension_fila=str(chr(dimension_tablero+48))
+    dimension_columna=str(chr(dimension_tablero+64))
 
     while(validacion==False):
-        casilla = raw_input("Ingrese la casilla de la luz que desea apagar ")
+        casilla = input("Ingrese la casilla de la luz que desea apagar ")
         casilla = casilla.upper()
         if(len(casilla)==2):
             if((casilla[0]>='A') and (casilla[0]<=dimension_columna)):
