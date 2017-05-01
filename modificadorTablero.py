a00f1dbd90293c374280005078ecf6d23d178505
FedericoNery <fedegnery@gmail.com>
2017-05-01 15:37:46 -0300

    Mediante el ingreso de una casilla válida, se procede a realizar la modificación de la luz en su respectiva casilla y sus aledañas. La prueba se realizó con la generación de un tablero aleatorio

---
+++ modificadorTablero.py
@@ -0,0 +1,36 @@
+from niveles_aleatorios import generarTablerosConLuces
+from ingreso_de_casilla import validacionIngresoDeCasillero
+from mostrar_tablero import imprimir_Tablero
+def modificoTablero(Diccionario,Casilla):
+    #Cuatro casilleros alrededor del central
+    filaCasillaCentral=Casilla[1]
+    columnaCasillaCentral=Casilla[0]
+    """ Casilla Central"""
+    if(Diccionario.get(Casilla,[])=='O'):
+        Diccionario[Casilla]='.'
+    """ Casilla a la izquierda de la Central """
+    casillaALaIzquierda= chr(ord(columnaCasillaCentral)-1)+filaCasillaCentral
+    if(Diccionario.get(casillaALaIzquierda,[])):
+        Diccionario[casillaALaIzquierda]='.'
+    """ Casilla a la derecha de la Central """
+    casillaALaDerecha = chr(ord(columnaCasillaCentral)+1)+filaCasillaCentral
+    if(Diccionario.get(casillaALaDerecha, [])):
+        Diccionario[casillaALaDerecha]='.'
+    """Casilla superior a la Central"""
+    casillaSuperior= columnaCasillaCentral + str(int(filaCasillaCentral)-1)
+    if(Diccionario.get(casillaSuperior, [])=='O'):
+        Diccionario[casillaSuperior]='.'
+    """Casilla inferior a la Central"""
+    casillaInferior = columnaCasillaCentral + str(int(filaCasillaCentral)+1)
+    if(Diccionario.get(casillaInferior, [])=='O'):
+        Diccionario[casillaInferior]='.'
+    return (Diccionario)
+
+
+dimension_tablero=6
+diccionario=generarTablerosConLuces(dimension_tablero)
+imprimir_Tablero(diccionario,dimension_tablero)
+Casilla=validacionIngresoDeCasillero(dimension_tablero)
+diccionario=modificoTablero(diccionario,Casilla)
+imprimir_Tablero(diccionario,dimension_tablero)
+
