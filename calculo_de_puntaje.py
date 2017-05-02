c8f5e41e325e3bb34701a99b5f4f19ca1e7f5265
FedericoNery <fedegnery@gmail.com>
2017-05-02 18:25:07 -0300

    Recibe parámetros para calcular el puntaje por nivel y el puntaje total. Devuelve la lista con los puntajes por niveles, el puntaje por nivel y el puntaje total

--- calculo_de_puntaje.py
+++ calculo_de_puntaje.py
@@ -1,22 +1,12 @@
-def calculo_de_puntaje(puntaje_acumulado,nivel_actual,movimientos_restantes,estado_nivel,luces_prendidas,dimension_tablero):
-    puntaje_nivel=[]
-    if(movimientos_restantes==0):
-        puntaje_acumulado=puntaje_acumulado-300
-    if(estado_nivel=="Ganado"):
-        puntaje_acumulado=puntaje_acumulado+500
-        puntaje_nivel[nivel_actual]=puntaje_acumulado
-    elif(estado_nivel=="Reiniciado"):
-        puntaje_acumulado=puntaje_acumulado-50*luces_prendidas
-        movimientos_restantes=3*dimension_tablero
-
-    return (puntaje_acumulado,puntaje_nivel[nivel_actual])
-
-a=calculo_de_puntaje(100,1,0,"Ganado",0,5)
-print (a)
-a=calculo_de_puntaje(a,2,0,"Ganado",0,5)
-print (a)
-a=calculo_de_puntaje(a,3,0,"Ganado",0,5)
-print (a)
-a=calculo_de_puntaje(a,4,0,"Ganado",0,5)
-print (a)
-a=calculo_de_puntaje(a,5,0,"Ganado",0,5)
+def calculoPuntaje(ganonivel,nivel,lucesRestantes,reset,puntajeActual,puntajesPorNivel):
+    puntajeTotal=0
+    if(ganonivel==True):
+         puntajeActual=puntajeActual+500
+    elif(ganonivel==False):
+         puntajeActual=puntajeActual-300
+    if(reset==True):
+         puntajeActual=puntajeActual-50*lucesRestantes#Asegurarse que lucesRestantes sea un entero
+    puntajesPorNivel[nivel]=puntajeActual+puntajesPorNivel[nivel]
+    for i in puntajesPorNivel:
+        puntajeTotal=i+puntajeTotal
+    return(puntajesPorNivel[nivel],puntajeTotal,puntajesPorNivel)
