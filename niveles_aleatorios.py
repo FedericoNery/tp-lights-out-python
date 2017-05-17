a2dee59c881aad64f938acbf6f70dcda7d64e87b
FedericoNery <fedegnery@gmail.com>
2017-05-17 16:06:58 -0300

    Se realiza code review y refactory del archivo en cuestión. Cambio de nombres de algunas variables y eliminacion de líneas de código inecesarias

--- niveles_aleatorios.py
+++ niveles_aleatorios.py
@@ -1,27 +1,29 @@
 import random
-from mostrar_tablero import imprimir_Tablero
-def generarTablerosConLuces(dimension_tablero):
-    Diccionario = {}
-    listaValoresAleatorios=[]
-    listaPosiblesValores=['O','.']
-    j=0
-    cantidadDeCasillas=dimension_tablero**2
-    for i in range(cantidadDeCasillas):
-        valor=random.choice(listaPosiblesValores)
-        listaValoresAleatorios.append(valor)
-    random.shuffle(listaValoresAleatorios)
-    ultima_fila_numero = dimension_tablero
-    ultima_columna_letra = str(chr(dimension_tablero + 64))
-    fila = 1
-    columna = 'A'
-    while (columna <= ultima_columna_letra):  # Columna vale A
-        while (fila <= dimension_tablero):
-            Diccionario[columna + str(fila)] = listaValoresAleatorios[j]
-            j=j+1
-            fila = fila + 1
-        fila = 1
-        columna = ord(columna) + 1
-        columna = chr(columna)
+from mostrar_tablero import imprimirTablero
 
-    return (Diccionario)
+def generarTableroAleatorio(dimensionTablero):
+    tablero = {}
+    estadosAleatoriosDeLuces=[]
+    posiblesEstadosDeLuces=['O','.']
+    indiceEstadosAleatoriosDeLuces=0
+    cantidadTotalDeCasillas= dimensionTablero ** 2
+
+    for i in range(cantidadTotalDeCasillas):
+        estadoDefinidoDeLaLuz=random.choice(posiblesEstadosDeLuces)
+        estadosAleatoriosDeLuces.append(estadoDefinidoDeLaLuz)
+    random.shuffle(estadosAleatoriosDeLuces)
+    letraDeLaUltimaColumnaDelTablero = str(chr(dimensionTablero + 64))
+    numeroDeLaFilaAnalizada = 1
+    letraDeLaColumnaAnalizada = 'A'
+
+    while (letraDeLaColumnaAnalizada <= letraDeLaUltimaColumnaDelTablero):  # Columna vale A
+        while (numeroDeLaFilaAnalizada <= dimensionTablero):
+            tablero[letraDeLaColumnaAnalizada + str(numeroDeLaFilaAnalizada)] = estadosAleatoriosDeLuces[indiceEstadosAleatoriosDeLuces]
+            indiceEstadosAleatoriosDeLuces=indiceEstadosAleatoriosDeLuces+1
+            numeroDeLaFilaAnalizada = numeroDeLaFilaAnalizada + 1
+        numeroDeLaFilaAnalizada = 1
+        letraDeLaColumnaAnalizada = ord(letraDeLaColumnaAnalizada) + 1
+        letraDeLaColumnaAnalizada = chr(letraDeLaColumnaAnalizada)
+
+    return (tablero)
 
