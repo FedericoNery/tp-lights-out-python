8139ddecc227021009ff4d9f8ccd38ad9c98e23d
FedericoNery <fedegnery@gmail.com>
2017-05-17 16:22:14 -0300

    Se realiza code review y refactory del archivo en cuestión. Se realizo una mejora significativa a nivel estructural respecto de la función que realiza el archivo

--- modificadorTablero.py
+++ modificadorTablero.py
@@ -1,44 +1,29 @@
-from niveles_aleatorios import generarTablerosConLuces
+from niveles_aleatorios import generarTableroAleatorio
 from ingreso_de_casilla import validacionIngresoDeCasillero
-from mostrar_tablero import imprimir_Tablero
-def modificoTablero(Diccionario,Casilla):
+from mostrar_tablero import imprimirTablero
+
+def modificoTablero(tablero, ingresoCoordenadaDelUsuario):
     #Cuatro casilleros alrededor del central
-    filaCasillaCentral=Casilla[1]
-    columnaCasillaCentral=Casilla[0]
-    """ Casilla Central"""
-    if(Diccionario.get(Casilla,[])=='O'):
-        Diccionario[Casilla]='.'
-    else:
-        Diccionario[Casilla]='O'
-    """ Casilla a la izquierda de la Central """
-    casillaALaIzquierda= chr(ord(columnaCasillaCentral)-1)+filaCasillaCentral
-    if(casillaALaIzquierda in Diccionario):
-        if(Diccionario.get(casillaALaIzquierda,[])=='O'):
-            Diccionario[casillaALaIzquierda]='.'
-        else:
-            Diccionario[casillaALaIzquierda]='O'
-    """ Casilla a la derecha de la Central """
-    casillaALaDerecha = chr(ord(columnaCasillaCentral)+1)+filaCasillaCentral
-    if (casillaALaDerecha in Diccionario):
-        if(Diccionario.get(casillaALaDerecha,[])=='O'):
-            Diccionario[casillaALaDerecha]='.'
-        else:
-            Diccionario[casillaALaDerecha]='O'
-    """Casilla superior a la Central"""
-    casillaSuperior= columnaCasillaCentral + str(int(filaCasillaCentral)-1)
-    if (casillaSuperior in Diccionario):
-        if(Diccionario.get(casillaSuperior,[])=='O'):
-            Diccionario[casillaSuperior]='.'
-        else:
-            Diccionario[casillaSuperior]='O'
-    """Casilla inferior a la Central"""
-    casillaInferior = columnaCasillaCentral + str(int(filaCasillaCentral)+1)
-    if (casillaInferior in Diccionario):
-        if(Diccionario.get(casillaInferior, [])=='O'):
-            Diccionario[casillaInferior]='.'
-        else:
-            Diccionario[casillaInferior]='O'
-    return (Diccionario)
+    filaCasillaCentral = ingresoCoordenadaDelUsuario[1]
+    columnaCasillaCentral = ingresoCoordenadaDelUsuario[0]
+
+    casillaALaIzquierda = chr(ord(columnaCasillaCentral) - 1) + filaCasillaCentral
+    casillaALaDerecha = chr(ord(columnaCasillaCentral) + 1) + filaCasillaCentral
+    casillaSuperior = columnaCasillaCentral + str(int(filaCasillaCentral) - 1)
+    casillaInferior = columnaCasillaCentral + str(int(filaCasillaCentral) + 1)
+
+    casillasConPosiblesCambiosDeEstado=[ingresoCoordenadaDelUsuario,casillaALaIzquierda,casillaALaDerecha,casillaSuperior,casillaInferior]
+
+    for coordenada in casillasConPosiblesCambiosDeEstado:
+        if(coordenada in tablero):
+
+            if(tablero.get(coordenada,[]) == 'O'):
+                tablero[coordenada] = '.'
+
+            else:
+                tablero[coordenada] = 'O'
+
+    return (tablero)
 
 
 
