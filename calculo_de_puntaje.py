464094b693a62ffaf3d99e632b5601ed32ed5220
FedericoNery <fedegnery@gmail.com>
2017-05-18 18:21:04 -0300

    Se realiza code review y refactory del archivo en cuestión. Falta mejorar

--- calculo_de_puntaje.py
+++ calculo_de_puntaje.py
@@ -1,12 +1,37 @@
-def calculoPuntaje(ganonivel,nivel,lucesRestantes,reset,puntajeActual,puntajesPorNivel):
+def calculoPuntaje(ganoNivel, nivel, lucesRestantes, reinicioNivel, puntajeActual, puntajesPorNivel):
     puntajeTotal=0
-    if(ganonivel==True):
+    if(ganoNivel):
          puntajeActual=puntajeActual+500
-    elif(ganonivel==False):
+
+    elif(ganoNivel==False):
          puntajeActual=puntajeActual-300
-    if(reset==True):
-         puntajeActual=puntajeActual-50*lucesRestantes#Asegurarse que lucesRestantes sea un entero
+
+    if(reinicioNivel):
+         puntajeActual=puntajeActual-50*lucesRestantes
+
     puntajesPorNivel[nivel-1]=puntajeActual+puntajesPorNivel[nivel-1]
+
     for i in puntajesPorNivel:
         puntajeTotal=i+puntajeTotal
+
     return(puntajesPorNivel[nivel-1],puntajeTotal,puntajesPorNivel)
+
+
+def calculoPuntajeTotal(puntajesPorNivel):
+    for i in puntajesPorNivel:
+        puntajeTotal=i+puntajeTotal
+    return puntajeTotal
+
+def calculoPuntajeActual(ganoNivel,reinicioNivel,lucesRestantes):
+    if (ganoNivel):
+        return 500
+
+    elif (not ganoNivel):
+        return (-300)
+
+    if (reinicioNivel):
+        return (-50*lucesRestantes)
+
+
+
+
