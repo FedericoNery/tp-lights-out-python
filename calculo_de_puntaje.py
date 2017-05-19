55f581dbc8e604f1a72a18a526143104d853401b
FedericoNery <fedegnery@gmail.com>
2017-05-18 21:28:50 -0300

    Se realiza code review y refactory. Falta mejorar

--- calculo_de_puntaje.py
+++ calculo_de_puntaje.py
@@ -33,6 +33,14 @@ def calculoPuntajeActual(ganoNivel,reinicioNivel,lucesRestantes):
     if (reinicioNivel):
         return (-50*lucesRestantes)
 
+def imprimirEncabezadoTablaPuntajesFinales(puntajeTotal):
+    print("")
+    print("TABLA DE PUNTAJES FINALES: ")
+    print("El puntaje total es de: " + str(puntajeTotal))
 
+def imprimirTodosLosPuntajesDeLosNiveles(puntajesPorNivel,contadorNivel):
+    for i in puntajesPorNivel:
+        contadorNivel = contadorNivel + 1
+        print("El puntaje obtenido en el nivel " + str(contadorNivel) + " es de " + str(i))
 
 
