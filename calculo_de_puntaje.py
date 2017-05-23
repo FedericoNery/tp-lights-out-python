48f5ff2651849fbb52cb860a7a59afb2f48abedb
FedericoNery <fedegnery@gmail.com>
2017-05-23 18:12:50 -0300

    Se elimina un argumento de una función

--- calculo_de_puntaje.py
+++ calculo_de_puntaje.py
@@ -20,7 +20,8 @@ def calculoPuntajeActual(ganoNivel,reinicioNivel,lucesRestantes):
 def imprimirPuntajeTotal(puntajeTotal):
     print("El puntaje total es de: " + str(puntajeTotal))
 
-def imprimirTodosLosPuntajesDeLosNiveles(puntajesPorNivel,contadorNivel):
+def imprimirTodosLosPuntajesDeLosNiveles(puntajesPorNivel):
+    contadorNivel = 0
     for i in puntajesPorNivel:
         contadorNivel = contadorNivel + 1
         print("El puntaje obtenido en el nivel " + str(contadorNivel) + " es de " + str(i))
