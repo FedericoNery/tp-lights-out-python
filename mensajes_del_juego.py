978ded888a5099ce46df7bd3fb41e7ebc7288d04
FedericoNery <fedegnery@gmail.com>
2017-05-23 18:31:56 -0300

    Módulo con los mensajes que aparecen al finalizar el nivel, perder el nivel o ganar el juego

---
+++ mensajes_del_juego.py
@@ -0,0 +1,28 @@
+def mensajeGanoJuego():
+    print("""
+      G G G      A     NN    N   O O O      E E E  L        J J J U     U  E E E   G G G   O O O
+     G          A A    N N   N  O     O     E      L          J   U     U  E      G       O     O
+     G   G G   A   A   N  N  N  O     O     E E E  L          J   U     U  E E E  G   G G O     O
+     G     G  A A A A  N   N N  O     O     E      L          J   U     U  E      G     G O     O
+      G G G  A       A N    NN   O O O      E E E  L L L   J J     U U U   E E E   G G G   O O O
+    """)
+
+def mensajeGanoNivel():
+    print("""
+             G G G      A     NN    N   O O O      E E E  L        NN    N  I  V       V  E E E  L
+            G          A A    N N   N  O     O     E      L        N N   N  I   V     V   E      L
+            G   G G   A   A   N  N  N  O     O     E E E  L        N  N  N  I    V   V    E E E  L
+            G     G  A A A A  N   N N  O     O     E      L        N   N N  I     V V     E      L
+             G G G  A       A N    NN   O O O      E E E  L L L    N    NN  I      V      E E E  L L L
+            """)
+    return ("")
+
+def mensajePerdiste():
+    print("""
+           P P P   E E E  R R R    D D D   I   S S S  T T T  E E E
+           P    P  E      R    R   D    D  I  S         T    E
+           P P P   E E E  R R R    D    D  I    S S     T    E E E
+           P       E      R    R   D    D  I       S    T    E
+           P       E E E  R     R  D D D   I  S S S     T    E E E
+           """)
+    return ("")
