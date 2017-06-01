9b611b9d281e1e0c14574896805378c3c1eb3e2d
FedericoNery <fedegnery@gmail.com>
2017-06-01 15:55:48 -0300

    Se agrega el mensaje de bienvenida del juego.

--- mensajes_del_juego.py
+++ mensajes_del_juego.py
@@ -1,3 +1,18 @@
+def mensaje_de_bienvenida():
+    print("|- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -|")
+    print("| B B B    I   E E E  NN    N V       V E E E  NN    N  I  D D D     O O O          A     |")
+    print("| B   B    I   E      N N   N  V     V  E      N N   N  I  D     D  O     O        A A    |")
+    print("| B B B B  I   E E E  N  N  N   V   v   E E E  N  N  N  I  D     D  O     O       A   A   |")
+    print("| B     B  I   E      N   N N    V V    E      N   N N  I  D     D  O     O      A A A A  |")
+    print("| B B B B  I   E E E  N    NN     V     E E E  N    NN  I  D D D     O O O      A       A |")
+    print("|                                                                                         |")
+    print("| L      U     U   C C C  E E E   S S S      F F F  U     U  E E E  R R R       A         |")
+    print("| L      U     U  C       E      S           F      U     U  E      R    R     A A        |")
+    print("| L      U     U  C       E E E   S S S      F F F  U     U  E E E  R R R     A   A       |")
+    print("| L      U     U  C       E            S     F      U     U  E      R   R    A A A A      |")
+    print("| L L L   U U U    C C C  E E E   S S S      F       U U U   E E E  R    R  A       A     |")
+    print("|- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -|\n")
+
 def mensajeGanoJuego():
     print("""
       G G G      A     NN    N   O O O      E E E  L        J J J U     U  E E E   G G G   O O O
