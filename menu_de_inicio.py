1656197b0d2b64208278b3c2744a7b2e842e129a
FedericoNery <fedegnery@gmail.com>
2017-06-01 15:56:38 -0300

    Se modifican las opciones del menú para que el usuario pueda seleccionar el modo de juego de forma directa

--- menu_de_inicio.py
+++ menu_de_inicio.py
@@ -1,39 +1,10 @@
 import sys
 import logica_del_juego
-def mensaje_de_bienvenida():
-    print("|- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -|")
-    print("| B B B    I   E E E  NN    N V       V E E E  NN    N  I  D D D     O O O          A     |")
-    print("| B   B    I   E      N N   N  V     V  E      N N   N  I  D     D  O     O        A A    |")
-    print("| B B B B  I   E E E  N  N  N   V   v   E E E  N  N  N  I  D     D  O     O       A   A   |")
-    print("| B     B  I   E      N   N N    V V    E      N   N N  I  D     D  O     O      A A A A  |")
-    print("| B B B B  I   E E E  N    NN     V     E E E  N    NN  I  D D D     O O O      A       A |")
-    print("|                                                                                         |")
-    print("| L      U     U   C C C  E E E   S S S      F F F  U     U  E E E  R R R       A         |")
-    print("| L      U     U  C       E      S           F      U     U  E      R    R     A A        |")
-    print("| L      U     U  C       E E E   S S S      F F F  U     U  E E E  R R R     A   A       |")
-    print("| L      U     U  C       E            S     F      U     U  E      R   R    A A A A      |")
-    print("| L L L   U U U    C C C  E E E   S S S      F       U U U   E E E  R    R  A       A     |")
-    print("|- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -|\n")
+import mensajes_del_juego
+
 
 def dimensionIngresadaDelTableroValida(numeroDeLaDimensionDelTablero):
     return ((numeroDeLaDimensionDelTablero == '5') or (numeroDeLaDimensionDelTablero == '6')or (numeroDeLaDimensionDelTablero == '7') or (numeroDeLaDimensionDelTablero == '8') or (numeroDeLaDimensionDelTablero == '9')or (numeroDeLaDimensionDelTablero=='10'))
-
-def ingresoDeOpcionJugarSalir():
-
-    validacionNumeroIngresadoOpcion = False
-
-    while (not validacionNumeroIngresadoOpcion):
-
-        numeroDeOpcionElegida = input("Ingrese el valor numerico de lo que desea hacer ")
-
-        if (numeroDeOpcionElegida == '1'):
-            validacionNumeroIngresadoOpcion = True
-
-        elif (numeroDeOpcionElegida == '2'):
-            sys.exit()
-
-        else:
-            print("Error")
 
 def ingresoDeLaDimensionDelTablero():
 
@@ -53,14 +24,10 @@ def ingresoDeLaDimensionDelTablero():
             validacionDeLaDimensionElegidaDelTablero = False
             print("Error")
 
-def imprimirJugarSalir():
-    print("1.JUGAR ")
-    print("2.SALIR ")
-
-def imprimirAleatorioPredeterminado():
-    print("\n")
-    print("1.MODO ALEATORIO")
-    print("2.MODO PREDETERMINADO\n")
+def imprimirOpcionesDeJuegoYOpcionSalir():
+    print("1.JUGAR MODO ALEATORIO ")
+    print("2.JUGAR MODO PREDETERMINADO")
+    print("3.SALIR ")
 
 def imprimirDimensionesTablerosAleatorios():
     print("5. Tablero de 5x5")
@@ -76,6 +43,9 @@ def usuarioSeleccionoAleatorio(numeroDeOpcionElegida):
 def usuarioSeleccionoPredeterminado(numeroDeOpcionElegida):
     return numeroDeOpcionElegida == '2'
 
+def usuarioSeleccionoSalir(numeroDeOpcionElegida):
+    return numeroDeOpcionElegida == '3'
+
 def mostrarMenuDeInicio():
     """ Interfaz de inicio del juego para seleccionar entre modo aleatorio o predeterminado. Dentro del modo
     aleatorio se podrá seleccionar la dimension del tablero"""
@@ -83,10 +53,8 @@ def mostrarMenuDeInicio():
     dimensionTablero = 0
     modoDelJuego = 0
 
-    mensaje_de_bienvenida()
-    imprimirJugarSalir()
-    ingresoDeOpcionJugarSalir()
-    imprimirAleatorioPredeterminado()
+    mensajes_del_juego.mensaje_de_bienvenida()
+    imprimirOpcionesDeJuegoYOpcionSalir()
     validacionDeIngresoDelModoDeJuego = False
 
     while(not validacionDeIngresoDelModoDeJuego):
@@ -94,27 +62,18 @@ def mostrarMenuDeInicio():
         numeroDeOpcionElegida = input("Ingrese el numero correspondiente a la accion que desea realizar ")
 
         if(usuarioSeleccionoAleatorio(numeroDeOpcionElegida)):
-            validacionDeIngresoDelModoDeJuego = True
-            modoDelJuego = "Aleatorio"
             imprimirDimensionesTablerosAleatorios()
             dimensionTablero = ingresoDeLaDimensionDelTablero()
+            logica_del_juego.principalAleatorio(dimensionTablero)
 
         elif(usuarioSeleccionoPredeterminado(numeroDeOpcionElegida)):
-            validacionDeIngresoDelModoDeJuego = True
             print ("Se determino jugar al modo predeterminado \n")
-            modoDelJuego = "Predeterminado"
             dimensionTablero = 5
+            logica_del_juego.principalPredeterminado(dimensionTablero)
 
+        elif(usuarioSeleccionoSalir(numeroDeOpcionElegida)):
+            sys.exit()
         else:
             validacionDeIngresoDelModoDeJuego = False
             print ("Error")
 
-    ejecutarModoDeJuegoSeleccionado(modoDelJuego,dimensionTablero)
-
-def ejecutarModoDeJuegoSeleccionado(modoDelJuego,dimensionTablero):
-
-    if (modoDelJuego == "Predeterminado"):
-        logica_del_juego.principalPredeterminado(dimensionTablero)
-
-    elif (modoDelJuego == "Aleatorio"):
-        logica_del_juego.principalAleatorio(dimensionTablero)
