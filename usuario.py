cabc44a20898a86df699afca554da29217f72bdb
FedericoNery <fedegnery@gmail.com>
2017-06-01 20:31:18 -0300

    Se crea el módulo Usuario. No se define si se implementará

---
+++ usuario.py
@@ -0,0 +1,9 @@
+global nivelDelJuego
+global puntajeActual
+
+def pasarDeNivel(nivelDelJuego):
+    return nivelDelJuego+1
+
+def calculoPuntajeActual(puntosObtenidos):
+    return puntajeActual+puntosObtenidos
+
