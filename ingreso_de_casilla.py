326bcc766361ff7244db0109334bb006a24ef01f
FedericoNery <fedegnery@gmail.com>
2017-06-15 19:08:44 -0300

    Implementacion de variables globales

--- ingreso_de_casilla.py
+++ ingreso_de_casilla.py
@@ -1,13 +1,11 @@
-#numeroUltimaFilaDelTablero = ""
-#letraUltimaColumnaDelTablero = ""
+numeroUltimaFilaDelTablero = ""
+letraUltimaColumnaDelTablero = ""
 
 def validacionIngresoDeCasillero(dimensionTablero):
     """Recibe el parametro que indica la dimension del tablero. El usuario procede al ingreso de la casilla, luego
     se verifica que la casilla pertenezca al tablero y que el ingreso no sea erroneo (mas caracteres o la inversion
     del orden de ingreso letra-numero(numero-letra)"""
-   # global numeroUltimaFilaDelTablero,letraUltimaColumnaDelTablero
-
-
+    global numeroUltimaFilaDelTablero,letraUltimaColumnaDelTablero
     ingresoDeCoordenada = False
     numeroUltimaFilaDelTablero = str(chr(dimensionTablero + 48))
     letraUltimaColumnaDelTablero = str(chr(dimensionTablero + 64))
@@ -16,7 +14,7 @@ def validacionIngresoDeCasillero(dimensionTablero):
         coordenadaDelUsuario = input("Ingrese la casilla de la luz que desea apagar o escriba \"reinicio\" para volver al inicio del nivel ")
         coordenadaDelUsuario = coordenadaDelUsuario.upper()
 
-        if(longitudDeCoordenadaValida(coordenadaDelUsuario) and letraDeCoordenadaValida(coordenadaDelUsuario,letraUltimaColumnaDelTablero)and numeroDeCoordenadaValida(coordenadaDelUsuario,numeroUltimaFilaDelTablero)):
+        if(longitudDeCoordenadaValida(coordenadaDelUsuario) and letraDeCoordenadaValida(coordenadaDelUsuario) and numeroDeCoordenadaValida(coordenadaDelUsuario)):
             ingresoDeCoordenada = True
             return (coordenadaDelUsuario)
 
@@ -27,22 +25,15 @@ def validacionIngresoDeCasillero(dimensionTablero):
             ingresoDeCoordenada = False
             print ("Error ")
 
-
 def longitudDeCoordenadaValida(coordenadaDelUsuario):
     return (len(coordenadaDelUsuario) == 2)
 
 def ingresoDeCoordenadaEsReinicio(coordenadaDelUsuario):
     return (coordenadaDelUsuario == "REINICIO")
 
-#def letraDeCoordenadaValida(coordenadaDelUsuario):
-def letraDeCoordenadaValida(coordenadaDelUsuario, letraUltimaColumnaDelTablero):
+def letraDeCoordenadaValida(coordenadaDelUsuario):
     return ((coordenadaDelUsuario[0] >= 'A') and (coordenadaDelUsuario[0] <= letraUltimaColumnaDelTablero))
 
-#def numeroDeCoordenadaValida(coordenadaDelUsuario):
-def numeroDeCoordenadaValida(coordenadaDelUsuario, numeroUltimaFilaDelTablero):
+def numeroDeCoordenadaValida(coordenadaDelUsuario):
     return ((coordenadaDelUsuario[1] >= '1') and (coordenadaDelUsuario[1] <= numeroUltimaFilaDelTablero))
 
-"""
-def validacionDeCoordenada(coordenadaDelUsuario):
-    if(longitudDeCoordenadaValida(coordenadaDelUsuario)):
-"""
